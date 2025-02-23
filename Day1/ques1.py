96af60d8e32bdc7aa12acd77060a3ec6369c66ee
Ipshita Patel <ipshita2908@gmail.com>
2025-02-23 15:50:23 +0530

    Day1

---
+++ Day1/ques1.py
@@ -0,0 +1,7 @@
+#Welcome to the “100 Days of Code” challenge! Today is your first day, and it’s time to start tracking your progress. You need to create a simple program that takes in an input and checks whether it is the first day of your coding challenge or not. Your program should store the number of days you’ve completed so far and let you know if you’ve successfully completed Day 1.
+n=int(input())
+if n==1:
+    print("Congratulations! You've completed Day 1!")
+else:
+    print(f"You're on Day {n} of the challenge! Keep going!")
+    
